31ec1509eb97bd1da8390db0ff915716489bf541
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-04-29 19:39:47 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+// const { species: animais } = data;
+// const moradores = animais.map((item) => item.residents).map(()=>);
 
+function getAnimalsOlderThan(animal, age) {
+  return data.species
+    .filter((specie) => animal === specie.name)[0].residents
+    .every((morador) => morador.age >= age);
 }
 
+console.log(getAnimalsOlderThan('bears', 1));
 module.exports = getAnimalsOlderThan;
