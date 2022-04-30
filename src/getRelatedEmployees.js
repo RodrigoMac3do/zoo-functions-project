7388c7072b190e13c0b229e2976c343f7f2b8c55
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-04-29 22:41:14 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let gerente = false;
+  data.employees.forEach((employee) => {
+    if (employee.managers.includes(id)) gerente = true;
+  });
+  return gerente;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.reduce((acc, employee) => {
+      if (employee.managers.includes(managerId)) {
+        acc.push(`${employee.firstName} ${employee.lastName}`);
+      }
+      return acc;
+    }, []);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
-module.exports = { isManager, getRelatedEmployees };
+// console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d9600a8'));
+
+module.exports = {
+  isManager,
+  getRelatedEmployees,
+};
