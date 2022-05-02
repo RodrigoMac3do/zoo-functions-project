a481249586bf2928886c51a747faf35f924bdad6
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-05-01 22:36:34 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const primeiraEspecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animais = data.species.find((animal) => animal.id === primeiraEspecie).residents;
+  const maiorIdade = Math.max(...animais.map((idade) => idade.age));
+  return Object.values(animais.find((animal) => animal.age === maiorIdade));
 }
+getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83');
 
 module.exports = getOldestFromFirstSpecies;
