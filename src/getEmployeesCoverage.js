49742963ae647f7f1d68d032960cf7666f6d21bc
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-05-02 21:30:50 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,59 @@
 const data = require('../data/zoo_data');
+const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function procuraId(pessoa) {
+  const empregado = data.employees
+    .find((employee) => employee
+      .id === pessoa || employee.firstName === pessoa || employee.lastName === pessoa);
+  return empregado;
 }
+
+function pegarSpecies(responsavel) {
+  return responsavel.map((ids) => getSpeciesByIds(ids)[0].name);
+}
+
+function pegaLocation(responsavel) {
+  return responsavel.map((ids) => getSpeciesByIds(ids)[0].location);
+}
+
+function semArgumento() {
+  return data.employees.map((employee) => ({
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: pegarSpecies(employee.responsibleFor),
+    locations: pegaLocation(employee.responsibleFor),
+  }));
+}
+
+function retornaObjeto(obj) {
+  const primeiroObj = Object.values(obj)[0];
+  const primeiroNome = procuraId(primeiroObj);
+  const {
+    firstName,
+    lastName,
+    id,
+    responsibleFor,
+  } = primeiroNome;
+
+  const novoArray = {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: pegarSpecies(responsibleFor),
+    locations: pegaLocation(responsibleFor),
+  };
+  return novoArray;
+}
+// console.log(retornaObjeto());
+
+function getEmployeesCoverage(obj) {
+  if (!obj) return semArgumento();
+  const primeiroObj = Object.values(obj)[0];
+  if (data.employees.some((employee) => employee.id === primeiroObj
+  || employee.firstName === primeiroObj || employee.lastName === primeiroObj)) {
+    return retornaObjeto(obj);
+  }
+  throw new Error('Informações inválidas');
+}
+console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
 
 module.exports = getEmployeesCoverage;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,5 +4,5 @@ function getSpeciesByIds(...ids) {
   return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
